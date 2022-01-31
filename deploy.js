a1e79cb816a854b187aa92c8627add07ae049f7d
Kyle Wheeless <kyle@wheeless.dev>
2022-01-31 15:48:22 -0800

    test deployment

--- deploy.js
+++ deploy.js
@@ -102,7 +102,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '34.201.241.3',
+      host: '3.93.220.136',
       username: 'ubuntu',
       privateKey: 'labsuser.pem',
     })
